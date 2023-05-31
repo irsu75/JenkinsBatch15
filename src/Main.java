953556b8ae683df105dda5f29458bde7865fbb92
irsu75 <irinasukhanova01@gmail.com>
2023-05-30 21:47:05 -0400

    simple project

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException  {
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello world!");
+            System.out.println("Hello world");
             Thread.sleep(2000);
         }
 
